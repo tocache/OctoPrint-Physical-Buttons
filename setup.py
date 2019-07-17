8081554dda7b60dae1c30f753991edc046c08db5
Kalun Lau <klnlau@gmail.com>
2019-07-17 12:15:44 -0500

    Update setup.py
    forked

--- setup.py
+++ setup.py
@@ -21,13 +21,13 @@ plugin_version = "0.0.6"
 plugin_description = """Add a physical Pause and Stop button to OctoPrint"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "netlands"
+plugin_author = "tocache"
 
 # The plugin's author's mail address.
-plugin_author_email = "barry@netlands.com"
+plugin_author_email = "tech@tocache.org"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/netlands/OctoPrint-Physical-Buttons"
+plugin_url = "https://github.com/tocache/OctoPrint-Physical-Buttons"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
